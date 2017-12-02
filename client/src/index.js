7ed92f08cbd62d56dee10776b3eb64a2d07273e7
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-02 11:44:07 -0500

    remove service worker stuff

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore.js';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+unregister(); // remove any existing service worker installations - can add in later
 
 const store = configureStore();
 window.reduxStore = store;
@@ -15,4 +17,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
