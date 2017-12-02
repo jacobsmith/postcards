79828ed57c12deb4929cd15bb1528075ad971f8b
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-02 11:58:46 -0500

    use the live key

--- client/src/approval/approval.js
+++ client/src/approval/approval.js
@@ -119,8 +119,7 @@ class Approval extends Component {
             <StripeCheckout
               ComponentClass="div"
               token={this.onToken}
-              // stripeKey="pk_live_lzEl0T1QkJfJNGTu8mwlANIK"
-              stripeKey="pk_test_1fP6F3hjLbG2TbLXPuEL3jEx"
+              stripeKey="pk_live_lzEl0T1QkJfJNGTu8mwlANIK"
               amount={149}
               name="Postcard"
               description={`Send postcard to ${this.props.postcard.addresses.to.addressName}`}
