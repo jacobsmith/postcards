9c1e22fd8d786f6e3a016c07c579f126c83e3d13
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-03 14:31:18 -0500

    get the images margins right on mobile

--- client/src/review/messageReview.js
+++ client/src/review/messageReview.js
@@ -3,17 +3,17 @@ import EditInfo from './editInfo.js';
 
 const MessageReview = ({ message }) => {
   return (
-    <div className="MessageReviewContainer">
-      <div className="MessageReviewLabel">my message</div>
+    <div className="AddressReview">
 
-      <div style={{float: 'right'}}>
+      {/* Yes, this is using AddressReview classes because they work better. Dear future jacob, make it generic class names */}
+      <div className="AddressReview-Type">my message</div>
+
+      <div className="AddressReviewAndEdit">
+        <div className="Address">
+          <div>{message}</div>
+        </div>
         <EditInfo editLocation="/message" />
       </div>
-
-      <div className="MessageReview">
-        {message}
-      </div>
-
     </div>
   )
 }
