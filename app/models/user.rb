345453328fbdfa1f5e06ab060345e631c117e72a
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-05 14:48:03 -0500

    password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, nil: false
+  validates :password, length: { minimum: 8, maximum: 60 }
 end
