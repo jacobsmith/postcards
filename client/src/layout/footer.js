7a9b1a31ff715e126360511075bbf28fb58870fc
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-05 18:35:55 -0500

    link footer to admin section

--- client/src/layout/footer.js
+++ client/src/layout/footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer-container">
-        &copy; Say it with a Postcard. All Rights Reserved.
+        <Link to="/admin" style={{color: '#365B64'}}>
+          &copy; Say it with a Postcard. All Rights Reserved.
+      </Link>
       </div>
     )
   }
