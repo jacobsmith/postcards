a3e9264f503111ce10f22fc26e51ea3260d0c4fd
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-12-09 11:38:34 -0500

    try to reset the json response in sessions destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
     @current_user.update!(revocable_session_token: SecureRandom.uuid)
 
-    render json: { success: true }
+    render json: { success: true, bust: "cache_on_heroku" }
   end
 end
